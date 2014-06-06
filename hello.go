a6f2d9706e2d3ed484fc162b783436c64029ab9b
marcgel <marcgel@google.com>
2014-06-06 11:47:22 -0700

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package hello
 import (
     "fmt"
     "net/http"
+
+    "appengine"
+    "appengine/user"
 )
 
 func init() {
@@ -10,5 +13,23 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+    // [START get_current_user]
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    // [END get_current_user]
+    // [START if_user]
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    // [END if_user]
+    // [START output]
+    fmt.Fprintf(w, "Hello, %v!", u)
+    // [END output]
 }
