b91ceee7afb83ece48349d5cfb38c4844c4f88c5
Devon H. O'Dell <devon.odell@gmail.com>
2017-10-18 20:17:03 +0000

    part2: run aefix

--- hello.go
+++ hello.go
@@ -1,37 +1,37 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "appengine"
-    "appengine/user"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/user"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    // [START new_context]
-    c := appengine.NewContext(r)
-    // [END new_context]
-    // [START get_current_user]
-    u := user.Current(c)
-    // [END get_current_user]
-    // [START if_user]
-    if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return
-    }
-    // [END if_user]
-    // [START output]
-    fmt.Fprintf(w, "Hello, %v!", u)
-    // [END output]
+	// [START new_context]
+	c := appengine.NewContext(r)
+	// [END new_context]
+	// [START get_current_user]
+	u := user.Current(c)
+	// [END get_current_user]
+	// [START if_user]
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	// [END if_user]
+	// [START output]
+	fmt.Fprintf(w, "Hello, %v!", u)
+	// [END output]
 }
